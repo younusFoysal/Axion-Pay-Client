04c2b9d1f0d028c6403dfb1f6d9c1db7ba212386
younusFoysal <younus.faysal@gmail.com>
2024-07-17 19:42:53 +0600

    Block & Active users done

--- src/pages/Dashboard/Admin/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useAuth} from "../../../context/AuthContext.jsx";
 import useAxiosSecure from "../../../hooks/useAxiosSecure.jsx";
+import toast from "react-hot-toast";
 
 const ManageUsers = () => {
 
@@ -54,6 +55,33 @@ const ManageUsers = () => {
     }, [usersAgent]);
 
 
+    // Approve Cash in
+    const handleUpdateStatus = async (email, currentStatus) => {
+        console.log(email, currentStatus);
+
+        try {
+            const newStatus = !currentStatus; // Toggle the status
+
+            const response = await axiosSecure.patch(`/users/update/${email}`, {
+                status: newStatus,
+            });
+
+            console.log(response);
+
+            if (response.data.success) {
+                toast.success('User Status Updated.');
+            } else {
+                toast.error(response.data.message);
+            }
+        } catch (error) {
+            console.error('Error updating status:', error);
+            toast.error('Failed to update status: ' + (error.response?.data?.message || 'Server error'));
+        }
+    };
+
+
+
+
     return (
         <div className="bg-indigo-100 min-h-screen">
             <div className=" pt-10 px-4 md:px-10 pb-4 w-full items-center justify-center">
@@ -65,8 +93,8 @@ const ManageUsers = () => {
                         <div
                             className=" mx-auto bg-white rounded-lg overflow-hidden shadow-2xl transform hover:scale-105 transition-transform duration-300">
                             <div className="text-center py-6 bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-                                <h1 className="text-3xl font-bold">Transaction Management</h1>
-                                <p className="mt-2">Manage Cash-In and Cash-Out Requests</p>
+                                <h1 className="text-3xl font-bold">Users Management</h1>
+                                <p className="mt-2">Manage Users and Agents Accounts</p>
                             </div>
                             <div className="p-8">
                                 <div className="flex justify-center mb-6">
@@ -97,7 +125,7 @@ const ManageUsers = () => {
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Phone</th>
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Balance</th>
-                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Registration Time</th>
+                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Last Updated</th>
                                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
                                         </tr>
                                         </thead>
@@ -112,46 +140,50 @@ const ManageUsers = () => {
                                                     <td className="px-6 py-4 whitespace-nowrap">{request.phone}</td>
                                                     <td className="px-6 py-4 whitespace-nowrap">{request.balance.toFixed(2)}</td>
                                                     <td className="px-6 py-4 whitespace-nowrap">{new Date(request.timestamp).toLocaleString()}</td>
+
+
                                                     <td className="px-6 py-4 whitespace-nowrap">
 
                                                         {request.status === true ? <button
                                                                 className="btn btn-sm ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out"
-                                                                disabled={request.status === "approved" || request.status === "rejected"}
-                                                                // onClick={() => handleRejectIn(request._id, request.userEmail)}
+                                                                onClick={() => handleUpdateStatus(request.email, request.status)}
                                                             >
                                                                 Block Account
                                                             </button>
                                                             :
                                                             <button
                                                                 className="btn btn-sm px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"
-                                                                disabled={request.status === "approved" || request.status === "rejected"}
-                                                                // onClick={() => handleApproveIn(request._id, request.userEmail)}
+                                                                onClick={() => handleUpdateStatus(request.email, request.status)}
                                                             >
                                                                 Active Account
                                                             </button>}
 
                                                     </td>
+
+
                                                 </tr>))
                                         }
 
 
                                         </tbody>
-                                                </table>
-                                            )}
-                                        {tab === 'agent' && (
-                                            <table
-                                                className="min-w-full divide-y divide-gray-200 bg-gray-100 rounded-lg shadow-xl sm:rounded-lg">
-                                                <thead>
-                                                <tr>
-                                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-                                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Phone</th>
-                                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Balance</th>
-                                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Registration Time</th>
-                                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
-                                                </tr>
-                                                </thead>
-                                                <tbody className="bg-white divide-y divide-gray-200">
+                                    </table>
+                                )}
+                                {tab === 'agent' && (
+                                    <table
+                                        className="min-w-full divide-y divide-gray-200 bg-gray-100 rounded-lg shadow-xl sm:rounded-lg">
+                                        <thead>
+                                        <tr>
+                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Phone</th>
+                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Balance</th>
+                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Last
+                                                Updated
+                                            </th>
+                                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody className="bg-white divide-y divide-gray-200">
 
                                                 {
                                                     usersAgent.map((request, index) => (
@@ -162,25 +194,27 @@ const ManageUsers = () => {
                                                             <td className="px-6 py-4 whitespace-nowrap">{request.phone}</td>
                                                             <td className="px-6 py-4 whitespace-nowrap">{request.balance.toFixed(2)}</td>
                                                             <td className="px-6 py-4 whitespace-nowrap">{new Date(request.timestamp).toLocaleString()}</td>
+
+
                                                             <td className="px-6 py-4 whitespace-nowrap">
 
                                                                 {request.status === true ? <button
                                                                         className="btn btn-sm ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out"
-                                                                        disabled={request.status === "approved" || request.status === "rejected"}
-                                                                        // onClick={() => handleRejectIn(request._id, request.userEmail)}
+                                                                        onClick={() => handleUpdateStatus(request.email, request.status)}
                                                                     >
                                                                         Block Account
                                                                     </button>
                                                                     :
                                                                     <button
                                                                         className="btn btn-sm px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"
-                                                                        disabled={request.status === "approved" || request.status === "rejected"}
-                                                                        // onClick={() => handleApproveIn(request._id, request.userEmail)}
+                                                                        onClick={() => handleUpdateStatus(request.email, request.status)}
                                                                     >
                                                                         Active Account
                                                                     </button>}
 
                                                             </td>
+
+
                                                         </tr>))
                                                 }
 
